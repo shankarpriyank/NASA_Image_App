5e47d761e38b652bb729141a219b671c73b854c0
Priyank Shankar <shankarpriyank312002@gmail.com>
2023-03-16 17:13:15 +0530

    Setup CI checks

--- app/src/test/java/com/priyank/nasa_image_app/ExampleUnitTest.kt
+++ app/src/test/java/com/priyank/nasa_image_app/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.priyank.nasa_image_app
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
